9260d6b99f07376b248c43e72fad242283bdc5b5
Марат Тухтаров <108996479+Marat1988@users.noreply.github.com>
2024-06-08 22:40:13 +0200

    Update Хранимка.sql

--- Хранимка.sql
+++ Хранимка.sql
@@ -9,25 +9,24 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 ALTER PROCEDURE [dbo].[up_sel_OrderDelails]
-                  @NumberOrder INT
-                  AS
-
-                  SELECT p.Name AS Product, lo.Amount AS Amount, lo.DisplayPrice AS DisplayPrice,
-                         lo.ShippingPrice AS ShippingPrice,
-                         CASE WHEN DisplayPrice>=ShippingPrice
-	                          THEN ROUND(100.0-(lo.ShippingPrice*100.0/lo.DisplayPrice), 2)
-                         ELSE -ROUND(100.0-(lo.ShippingPrice*100.0/lo.DisplayPrice), 2) END AS Discount,
-                         ROUND(Amount * DisplayPrice, 2) AS DisplayCaseAmount,
-                         ROUND(Amount * ShippingPrice, 2) AS [AmountBasedOnShippingPrice],
-						 u.UserName AS UserName, u.FirstName + ' ' + u.LastName AS UserFullName,
-						 o.DateTimeCreate AS DateOrder, o.PhoneNumber AS Phone
-                 FROM LineOrders lo
-                 INNER JOIN Orders o ON lo.OrderId=o.Id
-				 INNER JOIN AspNetUsers u ON o.UserId=u.Id
-                 INNER JOIN Products p ON lo.ProductId=p.Id
-                 INNER JOIN Groups g ON p.GroupId=g.Id
-                 WHERE o.Id=@NumberOrder
-                 ORDER BY 1
+@NumberOrder INT
+AS
+SELECT p.Name AS Product, lo.Amount AS Amount, lo.DisplayPrice AS DisplayPrice,
+	lo.ShippingPrice AS ShippingPrice,
+        CASE WHEN DisplayPrice>=ShippingPrice
+	     THEN ROUND(100.0-(lo.ShippingPrice*100.0/lo.DisplayPrice), 2)
+        ELSE -ROUND(100.0-(lo.ShippingPrice*100.0/lo.DisplayPrice), 2) END AS Discount,
+ROUND(Amount * DisplayPrice, 2) AS DisplayCaseAmount,
+ROUND(Amount * ShippingPrice, 2) AS [AmountBasedOnShippingPrice],
+u.UserName AS UserName, u.FirstName + ' ' + u.LastName AS UserFullName,
+o.DateTimeCreate AS DateOrder, o.PhoneNumber AS Phone
+FROM LineOrders lo
+INNER JOIN Orders o ON lo.OrderId=o.Id
+INNER JOIN AspNetUsers u ON o.UserId=u.Id
+INNER JOIN Products p ON lo.ProductId=p.Id
+INNER JOIN Groups g ON p.GroupId=g.Id
+WHERE o.Id=@NumberOrder
+ORDER BY 1
 GO
 
 
